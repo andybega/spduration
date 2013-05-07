ec9679c0d360067e65572f0df0a221857e565fde
Andreas Beger <andreas.beger@duke.edu>
2013-05-06 20:57:56 -0400

    buildDuration() keep ongoing spells
    buildDuration() keeps ongoing spells and codes most of the duration
    related variables as NA. Duration version of the data should now match
    N with source panel data. Checked that "duration", "failure", etc. give
    the same results as the previous versions which dropped ongoing spells.

--- package/spduration/R/buildDuration.R
+++ package/spduration/R/buildDuration.R
@@ -12,15 +12,14 @@ buildDuration <- function(data, y, unitID, tID, freq="monthly", slice.last=FALSE
   # Need to order by date to id and drop ongoing spells
   res <- data[order(data[, unitID], data[, tID]), ]
   
-  # Mark failure (0, 1)
+  # Mark failure (0, 1, NA for ongoing)
   failure <- function(x) return(c(x[1], pmax(0, diff(x))))
   res$failure <- unlist(by(res[, y], res[, unitID], failure))
-  
-  # Drop ongoing (1->1) spells, keep only 0 and onset (0->1)
-  res <- subset(res, !(get(y)==1 & (failure==0 | is.na(failure))))
+  res$ongoing <- ifelse(res[, y]==1 & res$failure==0, 1, 0)
+  res$failure <- ifelse(res$ongoing==1, NA, res$failure)
   
   # Mark end of a spell and create unique ID
-  # A spell can end 3 ways: failure, right-censor because past observation
+  # A spell can end 3 ways: failure, right-censor because last observation
   # period, or right censor because state ceased to exist.
   res$temp.t <- res[, tID]
   res <- ddply(res, .variables=unitID, transform, end=max(temp.t)) 
@@ -32,52 +31,55 @@ buildDuration <- function(data, y, unitID, tID, freq="monthly", slice.last=FALSE
     res$end.spell <- ifelse(format(res[, tID], '%Y')==format(as.Date(res$end), '%Y'), 1, 0)
   }
   res$end.spell <- ifelse(res$failure==1, 1, res$end.spell)
-  res$spellID <- rev(cumsum(rev(res$end.spell)))
+  
+  # Hack to correct ongoing spells, then create spellID
+  spellIDhelper <- ifelse(is.na(res$end.spell), 0, res$end.spell)
+  res$spellID <- rev(cumsum(rev(spellIDhelper)))
+  res$spellID <- ifelse(res$ongoing==1, NA, res$spellID)
   
   # Was there a failure at the end of a spell?
-  failedspells <- res$spellID[res$failure==1]
+  failedspells <- res$spellID[res$failure==1 & res$ongoing==0]
   helper <- cbind(failedspells, 0)
   colnames(helper) <- c("spellID", "cured")
   
   # Create cure (c) variable
   res <- merge(res, helper, by=("spellID"), all.x=TRUE)
-  res$cured[is.na(res$cured)] <- 1
+  res$cured[is.na(res$cured) & res$ongoing==0] <- 1
   res$atrisk <- 1 - res$cured
   
   # Create censor variable
   res$censor <- ifelse(with(res, end.spell==1 & failure==0), 1, 0)
   
   # Create duration variable, need to order by spell ID and date!!!!
-  helper <- rep(1, dim(res)[1])
+  helper <- 1 - res$ongoing
   res <- res[order(res$spellID, res[, tID]), ]
-  res$duration <- unlist(by(helper, res$spellID, cumsum))
+  res$duration[!is.na(res$spellID)] <- unlist(
+    by(helper[!is.na(res$spellID)], res$spellID[!is.na(res$spellID)], cumsum)
+    )
   res$t.0 <- res$duration - 1 ## previous duration
   
   # Slice option for forecast data; for CRISP/ICEWS
   if (slice.last==TRUE) {
     dataend = max(data[, tID])
-    pred.data <- subset(res, date==dataend)
+    pred.data <- res[res[, tID]==dataend, ]
     missing <- setdiff(unique(data[, unitID]), unique(pred.data[, unitID]))
-    missing <- data[data[, tID]==dataend & data[, unitID] %in% missing, ]
-    missing <- cbind(spellID=rep(NA, dim(missing)[1]), missing)
-    missing$failure <- 0
-    missing$end <- NA
-    missing$end.spell <- NA
-    missing$cured <- 0
-    missing$atrisk <- 1
-    missing$censor <- 1
-    missing$duration <- 1
-    res <- rbind(pred.data, missing)
+    if (length(missing)==0) {
+      missing <- data[data[, tID]==dataend & data[, unitID] %in% missing, ]
+      missing <- cbind(spellID=rep(NA, dim(missing)[1]), missing)
+      missing$failure <- 0
+      missing$ongoing <- 1
+      missing$end <- NA
+      missing$end.spell <- NA
+      missing$cured <- 0
+      missing$atrisk <- 1
+      missing$censor <- 1
+      missing$duration <- 1
+      res <- rbind(pred.data, missing)
+    }
   }
   
   # Reorder res by ccode/date
   res <- res[order(res[, unitID], res[, tID]), ]
+  res <- res[, !colnames(res) %in% "end"]
   return(res)
 }
-
-#test <- data.frame(event=c(0,0,0,1, 0,0,1,0), ccode=c(rep(1,4), rep(2,4)), month=as.Date(rep(c("2000-01-01","2000-02-01","2000-03-01","2000-04-01"),2)), stringsAsFactors=F)
-#buildDuration(test, "event", "ccode", "month", freq="monthly")
-#test <- data.frame(event=c(0,0,0,1, 0,0,1,0), ccode=c(rep(1,4), rep(2,4)), year=as.Date(rep(c("2000-01-01","2001-01-01","2002-03-01","2003-04-01"),2)), stringsAsFactors=F)
-#buildDuration(test, "event", "ccode", "year")
-#test <- data.frame(event=c(NA,0,0,1, 0,0,1,0), ccode=c(rep(1,4), rep(2,4)), year=as.Date(rep(c("2000-01-01","2001-01-01","2002-03-01","2003-04-01"),2)), stringsAsFactors=F)
-#buildDuration(test, "event", "ccode", "year")
