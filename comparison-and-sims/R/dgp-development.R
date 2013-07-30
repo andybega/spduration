5fb3c7d161f9546ff73d415a93706da78203b1d8
Andreas Beger <andreas.beger@duke.edu>
2013-07-31 00:00:12 +0900

    censored weibull regression

--- comparison-and-sims/R/dgp-development.R
+++ comparison-and-sims/R/dgp-development.R
@@ -14,17 +14,33 @@ library(survival)
 set.seed(1)
 N <- 1000
 n.param <- 3
-x <- cbind(x1=rnorm(N), x2=rnorm(N))
-b <- rnorm(n.param)
+x <- cbind(x1=rnorm(N, mean=2, sd=0.5), x2=rnorm(N, mean=2, sd=0.5))
+b <- rnorm(n.param, mean=1, sd=0.3)
 xb <- cbind(rep(1, N), x) %*% b
 mu <- exp(xb)
-y <- rweibull(N, shape=2, scale=mu)
+y <- rweibull(N, shape=5, scale=mu)
+
 model <- survreg(Surv(y, rep(1, N)) ~ x, dist="weibull")
 summary(model)
 
 
 # Weibull regression with censoring ---------------------------------------
 
+set.seed(1)
+N <- 1000
+n.param <- 3
+x <- cbind(x1=rnorm(N, mean=2, sd=0.5), x2=rnorm(N, mean=2, sd=0.5))
+b <- rnorm(n.param, mean=1, sd=0.3)
+xb <- cbind(rep(1, N), x) %*% b
+mu <- exp(xb)
+y <- rweibull(N, shape=5, scale=mu)
+
+censored.time <- rweibull(N, shape=5, scale=mean(mu))
+y.cen <- pmin(y, censored.time)
+di <- as.numeric(y<=censored.time)
+
+model <- survreg(Surv(y.cen, di) ~ x, dist="weibull")
+summary(model)
 
 # Weibull regression with TVC ---------------------------------------------
 
