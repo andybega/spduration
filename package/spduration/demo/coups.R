0cc558f77b9d118439d71b91b30851e29d943774
cassyld <cassyld@gmail.com>
2013-11-02 14:36:32 -0400

    rewrote demo code for coups but it breaks at like 30

--- package/spduration/demo/coups.R
+++ package/spduration/demo/coups.R
@@ -1,35 +1,71 @@
 ##
 ##    Demo/test code for "spduration" R package using Powell & Thyne coups
 ##    Andreas Beger
+##    Cassy Dorff
 ##    October 2013
 ##
 
-# all of the stuff below is old and needs to be changes
 
 # 1. install the latest version of spduration on dropbox
 # 2. update this demo file so it uses the coups stuff
 # 3. try out methods for spdurCRISP (model2 at the bottom), and see what breaks
 
-##########
-# What: Demo/test code for spdur dev
-# Date: November 2012
-# Who:  Andreas Beger
-###########
 
 library(spduration)
-
+## 
 # Task: create coups model and try out some of the methods
 data("coups")
 
 # Demo model to save estimation for examples
-dur.coup <- buildDuration(coups, "succ.coup", unitID='gwcode', tID='year',
-                          freq="year")
+duration.coup <- buildDuration(coups, "succ.coup", unitID='gwcode', tID='year',
+                          freq="yearly")
+duration.coup <- duration.coup[!is.na(duration.coup$failure), ]                          
 
-## Split duration model of coups
-model.coups <- spdur(duration ~ polity2, atrisk ~ polity2, data=dur.coup)
+# Split duration model of coups
+model.coups <- spdur(duration ~ polity2, atrisk ~ polity2, last='end.spell', data=dur.coup, distr="weibull", max.iter=300)
 
-## Build duration version from country-month dataset
-# data(insurgency)
+# summary methods
+summary(model.coups)
+
+# predict
+duration.coup$atrisk.hat <- predict(model.coups)
+top5 <- duration.coup[duration.coup$date==as.Date('2011-09-01'), c('country', 'atrisk.hat')]
+top5 <- top5[order(top5$atrisk.hat, decreasing=TRUE), ]
+head(top5)
+
+### CRISP wrapper (spdurCRISP)
+# # training: buildDuration, subset the coups through 2000
+# # test: buildDuration with coups through 2009, then subset it by dropping all before 2001
+# # pred: slice the last year of coups 2010
+# 
+# # Test CRISP/ICEWS wrapper
+# model.coups2 <- spdurCrisp(
+#   duration ~ polity2, atrisk ~ polity2,
+#   last='end.spell', train=duration.ins, test=duration.coup[1,], 
+#   pred=duration.coup[1,], distr="weibull", iter=300)
+# 
+# # try out methods
+# summary(model.coups2)
+
+# nobs(model.coup2)
+# AIC(model.coup2)
+# BIC(model.coup2)
+
+
+
+
+
+
+
+
+
+
+
+
+#-----------old--------------
+
+#Build duration version from country-month dataset
+#data(insurgency)
 # duration.ins <- buildDuration(insurgency, 'insurgency', unitID='ccode', tID='date')
 # duration.ins <- duration.ins[!is.na(duration.ins$failure), ]
 # 
@@ -51,23 +87,3 @@ model.coups <- spdur(duration ~ polity2, atrisk ~ polity2, data=dur.coup)
 # # Plot method
 # library(separationplot)
 # p <- plot(model)
-# 
-# ### CRISP wrapper (spdurCRISP)
-# # training: buildDuration, subset the coups through 2000
-# # test: buildDuration with coups through 2009, then subset it by dropping all before 2001
-# # pred: slice the last year of coups 2010
-# 
-# # Test CRISP/ICEWS wrapper
-# model2 <- spdurCrisp(
-#   duration ~ low_intensity + high_neighbors + exclpop.l1,
-#   atrisk ~ excl_groups_count.l1 + high_neighborhood + high_intensity + 
-#     exclpop.l1 + lgdppc.l1,
-#   last='end.spell', train=duration.ins, test=duration.ins[1,], 
-#   pred=duration.ins[1,], distr="weibull", iter=300)
-# 
-# # try out methods
-# summary(model2)
-# # ran ok, output is good/error message
-# nobs(model2)
-# AIC(model2)
-# BIC(model2)
