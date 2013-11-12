0632c3868c52fada2c73657d64b4f476f3b23bc6
cassyld <cassyld@gmail.com>
2013-11-12 09:43:59 -0500

    spduration functions

--- package/spduration/demo/spdurationTemp.R
+++ package/spduration/demo/spdurationTemp.R
@@ -17,23 +17,27 @@ library(spduration)
 # Task: create coups model and try out some of the methods
 data("coups")
 
+
 # Demo model to save estimation for examples
-duration.coup <- buildDuration(coups, "succ.coup", unitID="gwcode", tID='year',
+duration.coup <- spduration::buildDuration(coups, "succ.coup", unitID="gwcode", tID='year',
                           freq="year")                    
 
 # Split duration model of coups
-model.coups <- spdur(duration ~ polity2, atrisk ~ polity2, last='end.spell', 
+model.coups <- spduration::spdur(duration ~ polity2, atrisk ~ polity2, last='end.spell', 
                      data=duration.coup, distr="weibull", max.iter=300)
 
 # summary methods
-summary(model.coups) #works!
-nobs(model.coups) # works
-AIC(model.coups) #works!
-BIC(model.coups) #works!
+# Error: 'summary' is not an exported object from 'namespace:spduration'
+# Same error for nobs, AIC, BIC & Predict
+
+spduration::summary(model.coups) #works!
+spduration::nobs(model.coups) # works
+spduration::AIC(model.coups) #works!
+spduration::BIC(model.coups) #works!
 
 # predict
 pred <- duration.coup[-model.coups$na.action, ]
-pred$yhat <- predict(model.coups)
+pred$yhat <- spduration::predict(model.coups)
 top5 <- pred[pred$year==2010, c('gwcode', "year", 'yhat')]
 top5 <- top5[order(top5$yhat, decreasing=TRUE), ]
 head(top5)
@@ -42,12 +46,12 @@ head(top5)
 ### CRISP wrapper (spdurCRISP)
 # training: buildDuration, subset the coups through 2000
 coups.train <- coups[coups$year<="2000-06-30",]
-duration.coups.train <- buildDuration(coups.train, "succ.coup", unitID="gwcode", 
+duration.coups.train <- spduration::buildDuration(coups.train, "succ.coup", unitID="gwcode", 
                                       tID='year', freq="year")
 
 # test: buildDuration with coups through 2009
 coups.test <- coups[coups$year<="2009-06-30",]
-duration.coups.test <- buildDuration(coups.test, "succ.coup", unitID="gwcode", 
+duration.coups.test <-spduration::buildDuration(coups.test, "succ.coup", unitID="gwcode", 
                                      tID='year', freq="year")
 
 # then subset it by dropping all before 2001
@@ -55,20 +59,8 @@ duration.coups.test <- duration.coups.test[duration.coups.test$year>=2001, ]
 
 #pred: slice the last year of coups 2010
 coups.prediction<-coups[coups$year>="2010-06-30",]
-duration.coups.prediction<- buildDuration(coups.prediction, "succ.coup", unitID="gwcode", tID='year', freq="year")
+duration.coups.prediction<- spduration::buildDuration(coups.prediction, "succ.coup", unitID="gwcode", tID='year', freq="year")
 duration.coups.prediction<- duration.coups.prediction[complete.cases(duration.coups.prediction), ]
-
-# # Test CRISP/ICEWS wrapper
-model.coups2 <- spdurCrisp(
-duration ~ polity2, atrisk ~ polity2,
-last='end.spell', train=duration.coups.train, test=duration.coups.test[1,], 
-pred=duration.coups.prediction[1,], distr="weibull", iter=300)
-
-# try out methods
-summary(model.coups2)
-nobs(model.coups2)
-AIC(model.coups2)
-BIC(model.coups2)
 
 #plots
 plot(model.coups)
