057b9a64f3eb6fd0ae21ec2d8a0c2db714631417
Andreas Beger <adbeger@gmail.com>
2017-08-02 10:54:24 +0300

    update Rcpp function registration
    - should fix build break

--- src/init.c
+++ src/init.c
@@ -8,16 +8,16 @@
 */
 
 /* .Call calls */
-extern SEXP spduration_loglog_lnl(SEXP, SEXP, SEXP);
-extern SEXP spduration_sploglog_lnl(SEXP, SEXP, SEXP, SEXP);
-extern SEXP spduration_spweib_lnl(SEXP, SEXP, SEXP, SEXP);
-extern SEXP spduration_weib_lnl(SEXP, SEXP, SEXP);
+extern SEXP _spduration_loglog_lnl(SEXP, SEXP, SEXP);
+extern SEXP _spduration_sploglog_lnl(SEXP, SEXP, SEXP, SEXP);
+extern SEXP _spduration_spweib_lnl(SEXP, SEXP, SEXP, SEXP);
+extern SEXP _spduration_weib_lnl(SEXP, SEXP, SEXP);
 
 static const R_CallMethodDef CallEntries[] = {
-  {"spduration_loglog_lnl",   (DL_FUNC) &spduration_loglog_lnl,   3},
-  {"spduration_sploglog_lnl", (DL_FUNC) &spduration_sploglog_lnl, 4},
-  {"spduration_spweib_lnl",   (DL_FUNC) &spduration_spweib_lnl,   4},
-  {"spduration_weib_lnl",     (DL_FUNC) &spduration_weib_lnl,     3},
+  {"_spduration_loglog_lnl",   (DL_FUNC) &_spduration_loglog_lnl,   3},
+  {"_spduration_sploglog_lnl", (DL_FUNC) &_spduration_sploglog_lnl, 4},
+  {"_spduration_spweib_lnl",   (DL_FUNC) &_spduration_spweib_lnl,   4},
+  {"_spduration_weib_lnl",     (DL_FUNC) &_spduration_weib_lnl,     3},
   {NULL, NULL, 0}
 };
 
